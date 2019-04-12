60fdf6a5a0d9656ce44e53689d12db01182e1f2b
FooVoo <aiiiotus@bk.ru>
2019-04-12 14:18:20 +0400

    Added sort
    Fixed json routes

--- js/main.js
+++ js/main.js
@@ -6,47 +6,53 @@ request.send();
 
 var json = request.response;
 var jsonObj = JSON.parse(json);
+    jsonObj = jsonObj.results;
 
 function userCardAssembler(i) {
-    var userImg = jsonObj['results'][i]['picture']['medium'];
-    var userTitle = jsonObj['results'][i]['name']['title'];
-    var userFirst = jsonObj['results'][i]['name']['first'];
-    var userLast = jsonObj['results'][i]['name']['last'];
+    var userImg = jsonObj[i].picture.medium;
+    var userTitle = jsonObj[i].name.title;
+    var userFirst = jsonObj[i].name.first;
+    var userLast = jsonObj[i].name.last;
+    var userFirstLetter = userFirst[0];
     var userName = '<p>' + userTitle + '. ' + userFirst + ' ' + userLast + '</p>';
     var card = '<img src=' + userImg + '></img>' + userName + '<button onclick="showUserInfo(' + i +')">Show more Info</button>';
-    
-    return card;
+
+    return {'card' : card, 'userFirstLetter' : userFirstLetter};
 }
 
 function userInfoCardAssembler(i) {
-    var userImg = jsonObj['results'][i]['picture']['large'];
-    var userTitle = jsonObj['results'][i]['name']['title'];
-    var userFirst = jsonObj['results'][i]['name']['first'];
-    var userLast = jsonObj['results'][i]['name']['last'];
-    var userLocationStreet = jsonObj['results'][i]['location']['street'];
-    var userLocationCity = jsonObj['results'][i]['location']['city'];
-    var userLocationState = jsonObj['results'][i]['location']['state'];
-    var userEmail = jsonObj['results'][i]['email'];
-    var userTel = jsonObj['results'][i]['phone'];
+    var userImg = jsonObj[i].picture.large;
+    var userTitle = jsonObj[i].name.title;
+    var userFirst = jsonObj[i].name.first;
+    var userLast = jsonObj[i].name.last;
+    var userLocationStreet = jsonObj[i].location.street;
+    var userLocationCity = jsonObj[i].location.city;
+    var userLocationState = jsonObj[i].location.state;
+    var userEmail = jsonObj[i].email;
+    var userTel = jsonObj[i].phone;
     var userName = '<p>' + userTitle + '. ' + userFirst + ' ' + userLast + '</p>';
     
-    card = '<img src=' + userImg + '></img>' + userName;
+    card = '<img src=' + userImg + ' alt="User Photo"></img>' + userName;
     card += '<p>Street: ' + userLocationStreet +'</p><p>City: ' + userLocationCity + '</p><p>State: ' + userLocationState + '</p>';
     card +='<p>Email: ' + userEmail + '</p>' + '<p>Phone: ' + userTel + '</p>';
+    
     return card;
 }
 
 function addCard(text) {
     var childElem = document.createElement('article');
     var parentElem = document.getElementById('userList');
-    childElem.innerHTML = text;
+    childElem.innerHTML = text.card;
     childElem.className = 'user_profile';
+    childElem.id = text.userFirstLetter;
     parentElem.appendChild(childElem);
 }
 
 function showUsers() {
-    for (let i = 0; i < jsonObj['results'].length; i++) {
+    for (i = 0; i < jsonObj.length; i++) {
         addCard(userCardAssembler(i));
+        //elemArray = document.getElementsByClassName('user_profile');
+        //elemArray[i].id = i;
     }
 }
 
@@ -72,4 +78,45 @@ function dialogClose(){
     var dialogWindow = document.getElementById('dialog');
     removeElem('dialog');
     dialogWindow.close();
+}
+
+function deleteCards() {
+    var parentElem = document.getElementById('userList');
+    parentElem.innerHTML = '';
+}
+
+function sort() {
+    var sortStart = document.getElementById('sort').value;
+    deleteCards();
+    switch (sortStart) {
+        case 'A':
+            jsonObj.sort(function (obj1, obj2) {
+                if (obj1.name.first < obj2.name.first) {
+                    return -1;
+                }
+                if (obj1.name.first > obj2.name.first) {
+                    return 1;
+                }
+                return 0;
+            });
+            break;
+        case 'Z':
+            jsonObj.sort(function (obj1, obj2) {
+                if (obj1.name.first > obj2.name.first) {
+                    return -1;
+                }
+                if (obj1.name.first < obj2.name.first) {
+                    return 1;
+                }
+                return 0;
+            });
+            break;
+
+        default:
+
+
+            break;
+    }
+    
+    showUsers();
 }
